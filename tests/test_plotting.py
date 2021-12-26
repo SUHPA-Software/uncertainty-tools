e8feadfcf22f60cae549bc8c70b0903cede4196c
Yaseen Reza <57412770+yaseen157@users.noreply.github.com>
2021-12-26 17:19:33 +0000

    Update and rename test_uvisualisation.py to test_plotting.py

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -7,7 +7,7 @@ Created on Sat Dec 18 11:12:46 2021
 import numpy as np
 from uncertainties import unumpy, ufloat
 
-from plotting import create_ufloatmesh, plot_ufloatmesh
+from uvisualisation.plotting import create_ufloatmesh, plot_ufloatmesh
 
 
 def test_main():
